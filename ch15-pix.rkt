1e688f4e197d868142827fc30d8a4086c9656524
claytonbivens <51036884+claytonbivens@users.noreply.github.com>
2021-06-24 09:22:53 -0500

    Create ch15-pix.rkt

---
+++ ch15-pix.rkt
@@ -0,0 +1,24 @@
+(require picturing-programs)
+(provide (all-defined-out))
+
+(define black-pants
+  .)
+
+(define navy-pants
+  .)
+
+(define white-shirt
+  .)
+
+(define pink-shirt
+  .)
+
+(define pink-shoes
+  .)
+
+(define burgundy-shoes
+  .)
+
+(define navy-shoes
+  .)
+
